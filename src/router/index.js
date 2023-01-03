fcde58374a39911c7f797de5190c3c5e55497110
tenggi <rrohaman760@gmail.com>
2023-01-03 19:59:18 +0700

    convert html to vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/Home.vue'
+import ProductView from '../views/Product.vue'
+import ShoppingCart from '../views/ShoppingCart.vue'
+import SuccessView from '../views/Success.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +14,19 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/product',
+    name: 'product',
+    component: ProductView
+  },
+  {
+    path: '/cart',
+    name: 'cart',
+    component: ShoppingCart
+  },
+  {
+    path: '/success',
+    name: 'success',
+    component: SuccessView
   }
 ]
 
---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <HeaderShyna />
+    <HeroShyna />
+    <BannerShyna />
+    <InstagramShyna />
+    <ParthnerShyna />
+    <FooterShyna />
+  </div>
+</template>
+
+<script>
+import HeaderShyna from '@/components/HeaderShyna.vue';
+import HeroShyna from '@/components/HeroShyna.vue';
+import BannerShyna from '@/components/BannerShyna.vue';
+import InstagramShyna from '@/components/InstagramShyna.vue';
+import ParthnerShyna from '@/components/ParthnerShyna.vue';
+import FooterShyna from '@/components/FooterShyna.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    HeaderShyna,
+    HeroShyna,
+    BannerShyna,
+    InstagramShyna,
+    ParthnerShyna,
+    FooterShyna
+  }
+}
+</script>
